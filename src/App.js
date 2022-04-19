9db469efc34f304d17a97ba796849b6288cf4653
dewslyse <dewslyse@gmail.com>
2022-04-19 21:09:32 +0000

    App component changed to class-based component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
+import React, { PureComponent } from 'react';
 import Calculator from './components/Calculator';
 
-function App() {
-  return (
-    <div className="App">
-      <Calculator />
-    </div>
-  );
+class App extends PureComponent {
+  render() {
+    return (
+      <div className="App">
+        <Calculator />
+      </div>
+    );
+  }
 }
 
 export default App;
