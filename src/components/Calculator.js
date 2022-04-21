9bb084d6bf8fc7885242961357b11cdf50b700f9
dewslyse <dewslyse@gmail.com>
2022-04-21 17:36:16 +0000

    Add logic to Calculator

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,34 +1,54 @@
-/* eslint-disable linebreak-style */
-/* eslint-disable react/jsx-indent */
-
 import React, { PureComponent } from 'react';
 import Button from './Button';
+import calculate from '../logic/calculate';
+import Display from './Display';
 import './Calculator.scss';
 
 export default class Calculator extends PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      total: '0',
+      next: '0',
+      operation: null,
+    };
+  }
+
+  onClick = (e) => {
+    const newValue = calculate(this.state, e.target.innerText);
+    this.setState(newValue);
+    const outPut = document.getElementById('output');
+    if (newValue.next !== null) {
+      outPut.textContent = newValue.next;
+    } else {
+      outPut.textContent = newValue.total;
+    }
+  };
+
   render() {
     return (
       <div className="calculator">
-        <input id="output" type="text" value="0" />
-        <Button text="AC" />
-        <Button text="+/&ndash;" />
-        <Button text="%" />
-        <Button className="operators" text="&divide;" />
-        <Button text="7" />
-        <Button text="8" />
-        <Button text="9" />
-        <Button className="operators" text="&times;" />
-        <Button text="4" />
-        <Button text="5" />
-        <Button text="6" />
-        <Button className="operators" text="&ndash;" />
-        <Button text="1" />
-        <Button text="2" />
-        <Button text="3" />
-        <Button className="operators" text="+" />
-        <Button id="zero" text="0" />
-        <Button text="&bull;" />
-        <Button className="operators" text="=" />
+        <Display />
+        <Button text="AC" onClick={this.onClick} />
+        <Button text="+/-" onClick={this.onClick} />
+        <Button text="%" onClick={this.onClick} />
+        <Button className="operators" text="&divide;" onClick={this.onClick} />
+        <Button text="7" onClick={this.onClick} />
+        <Button text="8" onClick={this.onClick} />
+        <Button text="9" onClick={this.onClick} />
+        <Button className="operators" text="x" onClick={this.onClick} />
+        <Button text="4" onClick={this.onClick} />
+        <Button text="5" onClick={this.onClick} />
+        <Button text="6" onClick={this.onClick} />
+        <Button className="operators" text="-" onClick={this.onClick} />
+        <Button text="1" onClick={this.onClick} />
+        <Button text="2" onClick={this.onClick} />
+        <Button text="3" onClick={this.onClick} />
+        <Button className="operators" text="+" onClick={this.onClick} />
+        <Button id="zero" text="0" onClick={this.onClick} />
+        <Button text="." onClick={this.onClick} />
+        <Button className="operators" text="=" onClick={this.onClick} />
       </div>
     );
   }
