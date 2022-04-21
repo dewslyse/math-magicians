aad5641440b62022d7af323a15659cf200c6c2be
dewslyse <dewslyse@gmail.com>
2022-04-21 17:34:06 +0000

    Add Display component

---
+++ src/components/Display.js
@@ -0,0 +1,13 @@
+import React, { PureComponent } from 'react';
+
+class Display extends PureComponent {
+  render() {
+    return (
+      <div className="output">
+        <div id="output">0</div>
+      </div>
+    );
+  }
+}
+
+export default Display;
