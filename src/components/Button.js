d815bb05f8fb472691ce7446a1d94720d8379c12
dewslyse <dewslyse@gmail.com>
2022-04-21 17:32:12 +0000

    Add PropTypes to Button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,26 +1,25 @@
-/* eslint-disable linebreak-style */
-/* eslint-disable react/jsx-indent */
-/* eslint-disable indent */
-
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 
 class Button extends PureComponent {
-    render() {
-        const { className, id, text } = this.props;
+  render() {
+    const {
+      className, id, text, onClick,
+    } = this.props;
 
-        return (
-            <button type="button" className={className} id={id}>
-                {text}
-            </button>
-        );
-    }
+    return (
+      <button type="button" className={className} id={id} onClick={onClick}>
+        {text}
+      </button>
+    );
+  }
 }
 
 Button.propTypes = {
-    text: PropTypes.string.isRequired,
-    className: PropTypes.string.isRequired,
-    id: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
+  className: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired,
 };
 
 export default Button;
