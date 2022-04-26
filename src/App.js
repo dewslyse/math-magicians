2fbc4e59720495e6c4f1b2f2a4b41f251c324b62
dewslyse <dewslyse@gmail.com>
2022-04-26 23:44:56 +0000

    Add routing to application

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React, { PureComponent } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Nav from './components/Nav';
+import Home from './components/Home';
 import Calculator from './components/Calculator';
+import Quote from './components/Quote';
+import './App.scss';
 
 class App extends PureComponent {
   render() {
     return (
-      <div className="App">
-        <Calculator />
-      </div>
+      <BrowserRouter>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="calculator" element={<Calculator />} />
+          <Route path="quote" element={<Quote />} />
+        </Routes>
+      </BrowserRouter>
     );
   }
 }
